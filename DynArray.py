a920ac781bbd42af54ef82c589b2283bd164a7f6
sundar <sundar@rsundarrajan@live.com>
2016-06-07 09:28:31 +0530

    adding dynamic array py implementation with basic test and TC10 alone pass

---
+++ DynArray.py
@@ -0,0 +1,64 @@
+#!bin/python3
+
+import sys
+
+
+
+"""
+   Execute Query of form (1 x y) or (2 x y)
+   
+   SeqList , Querytype 1 or 2, x_val , y_val, lastAns,n
+
+
+   SeqList  -  Sequence nxn on which the Query has to be run
+   Query    -  Query either 1 or 2 based on which index has to be calculated
+   x_val    -  x value part of the query e.g 1 x y
+   y_val    -  y value part of the query
+   lastAns  -  last answer on which the update Query needs to be performed
+   n        -  n size
+
+"""
+
+def runQuery (seq,query,x,y,lastAns,n):
+   answer = 0
+   calc_index = (x ^ lastAns) % n;
+   if (query==1):
+      seq[calc_index].append(y)
+      return 0 
+   elif (query==2):
+      answer = seq[calc_index][y%n]
+      return answer
+
+
+"""
+   main() is not needed , added to simply block it
+   reads n,q and calls RunQuery with the sequence prepared already
+"""
+def main():
+   # read n , q  
+   n,q = input().strip().split(' ')
+   n,q = int(n),int(q)
+   lastAns = 0
+   answerlist =[]
+
+   querylist =[]
+   seq =[]
+   for t in range(n):
+      seq.append([])
+
+   for i in range(q):
+      qt,x,y = input().strip().split(' ')
+      qt,x,y = int(qt),int(x),int(y)
+      lastAns = runQuery(seq,qt,x,y,lastAns,n)
+      if (qt==2):
+         answerlist.append(lastAns)
+
+   for x in answerlist:
+      print(x)
+
+
+
+main()
+      
+       
+   
