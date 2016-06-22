9a8e328063fa3c62248295451d7258ab240bb8a9
helloworldsundar <helloworldsundar@gmail.com>
2016-06-22 09:46:16 +0530

    some changes in insertion sort

--- insertionsort.py
+++ insertionsort.py
@@ -5,15 +5,13 @@ def insertionsort(arr):
     for i in range(1, n):
         ccard = arr[i]
         for j in range(i, -1, -1):
+            print(j)
             if arr[j-1] > ccard:
+                #print (str(arr[j-1])+'comparing'+str(ccard))
                 arr[j] = arr[j-1]
-            else:
-                arr[j] = ccard
-                break
+                continue
+            arr[j] = ccard
     return arr
-
-
-
 
 def sort (arr,type):
     if(type=='insertionsort'):
