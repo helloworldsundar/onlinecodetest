4af6d1898886d39a6d61257e9111c49e9bc1b25f
helloworldsundar <helloworldsundar@gmail.com>
2016-06-24 09:48:24 +0530

    insertion sort still failing

--- insertionsort.py
+++ insertionsort.py
@@ -4,13 +4,15 @@ def insertionsort(arr):
     n = len(arr)
     for i in range(1, n):
         ccard = arr[i]
-        for j in range(i, -1, -1):
-            print(j)
+        print(ccard)
+        j = i
+        while (j >= 0):
             if arr[j-1] > ccard:
-                #print (str(arr[j-1])+'comparing'+str(ccard))
                 arr[j] = arr[j-1]
-                continue
-            arr[j] = ccard
+                j = j - 1
+            else:
+                break
+        arr[j] = ccard
     return arr
 
 def sort (arr,type):
