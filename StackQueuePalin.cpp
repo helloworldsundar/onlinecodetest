19f6bc40162e9bec1aae56e7b9110c35694899d1
sundar <sundar@rsundarrajan@live.com>
2016-07-03 20:25:56 +0530

    stack queue implementation.cpp

--- StackQueuePalin.cpp
+++ StackQueuePalin.cpp
@@ -8,10 +8,82 @@ class Solution {
 };
 
 
+/**
+class : Node
+
+represents Node Class with character instance
+& next instance of Node
+
+*/
+class Node
+{
+public:
+   char* m_data;
+   Node* m_next;
+
+   Node(){
+      m_data=new char('\0');
+      m_next=NULL;  
+   }
+   
+   Node(char c){
+      m_data=new char(c);
+      m_next=NULL;  
+   }
+   
+   char getData(){
+       return *(m_data);
+   }
+   
+   Node* getNext(){
+       return m_next;
+   }
+   
+   void setNext(Node* addr){
+       m_next=addr;
+   }
+   
+   void setData (char c){
+       *m_data=c;
+   }
+};
+/**
+class : Stack
+
+represents a Stack with LIFO implementation
+*/
 class Stack{
-   Node* top;
+public:
+   Node* m_top;
+
    Stack(){
-      `:q
+      m_top = NULL;
+   }
+   
+   void pushCharacter(char c){
+       // create the new node and push it to the top
+       Node* newNode = new Node(c);
+       
+       if (m_top == NULL){
+           m_top = newNode;
+       }
+       else{
+           Node* temp = m_top;
+           m_top = new Node (c);
+           m_top->setNext (temp);
+       }
+   }
+   
+   char popCharacter(void){
+       
+       if (m_top == NULL){
+           return '\0';
+       }
+       // reduce the stack by one by popping one character
+       Node* nextStack = m_top->getNext ();
+       char stack_data = m_top->getData ();
+       m_top = nextStack;
+       return (stack_data);
    }
 };
 //Below text , assume it is read only
